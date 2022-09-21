80fa299a9bd5f6fceed1f7df1fed92df850dae01
Isabella W <isabellawindhaber@gmail.com>
2022-09-21 13:06:36 +0200

    ..

---
+++ Isabella/Intermediate/index.js
@@ -0,0 +1,13 @@
+//Exercise 1
+
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+
+console.log(cars.sort());
+
+// Exercise 2
+
+const fruits = ["apple", "banana", "kiwi"];
+console.log(fruits.push("orange"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits);
