88ab74478c8b72645f79f2a6a5133d1b9a644ece
Isabella W <isabellawindhaber@gmail.com>
2022-09-21 14:28:23 +0200

    ..

---
+++ Isabella/Advanced/script.js
@@ -0,0 +1,17 @@
+let a = 7;
+
+let b = 14;
+
+let c = "21";
+
+
+let d = '36';
+
+
+let e = 42;
+
+
+let Sum = a+b+Number(c)+Number(d)+e;
+console.log(Sum);
+
+
--- Isabella/Intermediate/index.js
+++ Isabella/Intermediate/index.js
@@ -1,6 +1,6 @@
 //Exercise 1
 
-const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "BMW", "Ferrari"];
 
 console.log(cars.sort());
 
@@ -11,3 +11,17 @@ console.log(fruits.push("orange"));
 console.log(fruits);
 console.log(fruits.pop());
 console.log(fruits);
+
+const animals = ["monkey", "horse", "dog"];
+console.log(animals.sort());
+console.log(animals.unshift("cat"));
+console.log(animals);
+
+// Exercise 3
+
+const fruit = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+
+// var varia = fruit.split("/").join("<br>");
+let varia2 = fruit.replaceAll("/", "<br>");
+
+document.write(varia2);
