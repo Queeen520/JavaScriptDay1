49a8ea06f3c3a6f9316cc91817f1f43d831a396d
Isabella W <isabellawindhaber@gmail.com>
2022-09-21 15:17:52 +0200

    ...

--- Isabella/Advanced/script.js
+++ Isabella/Advanced/script.js
@@ -1,3 +1,6 @@
+// Exercise 1
+
+
 let a = 7;
 
 let b = 14;
@@ -13,5 +16,38 @@ let e = 42;
 
 let Sum = a+b+Number(c)+Number(d)+e;
 console.log(Sum);
+
+
+let f = '1';
+
+let g = 15;
+
+let h = 8;
+
+let i = "1";
+
+let sum1 = Number(f)*g*h*Number(i);
+console.log(sum1);
+
+console.log(Sum/sum1);
+document.write(Sum/sum1);
+
+
+let people = ["Greg", "Mary", "Devon", "James"];
+people.shift();
+console.log(people);
+
+people.unshift("Matt");
+console.log(people);
+
+people.pop();
+console.log(people);
+
+people.push("Isabella");
+console.log(people);
+console.log(people.slice(2,4));
+
+people.indexOf("Mary");
+
 
 
